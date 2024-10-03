b0ed73ee464d5188ecd50925a3535b3191a61781
Avaray <1459553+Avaray@users.noreply.github.com>
2024-10-03 13:32:05 +0200

    changed description

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const siteTitle = 'dav.one';
-export const siteDescription = 'Blog about AI, programming, and other stuff.';
+export const siteDescription = 'Blog about Stable Diffusion, Generative AI, and other stuff';
 export const OWNER = 'Dawid Wąsowski';
