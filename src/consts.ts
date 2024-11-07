89dbca71fe265489516b13a6d9bd8783a21bdf72
Avaray <1459553+Avaray@users.noreply.github.com>
2024-11-07 21:14:13 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const siteTitle = 'dav.one';
-export const siteDescription = 'Blog about Stable Diffusion, Generative AI, and other stuff';
+export const siteTitle = 'Blog about Stable Diffusion, Generative AI, and other stuff';
+export const siteDescription = "If you want to learn the basics or find interesting things related to Stable Diffusion, feel free to visit this blog. You'll find original, unique articles and tutorials here. ";
 export const OWNER = 'Dawid Wąsowski';
