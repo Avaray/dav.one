deb1e59dcfab927bfbc5a84b2db2ab8b0f9eb77f
Avaray <1459553+Avaray@users.noreply.github.com>
2024-12-07 16:04:21 +0100

    updated articles collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,17 +3,17 @@ import { getCollection } from 'astro:content';
 import { siteTitle, siteDescription } from '../consts';
 
 export async function GET(context) {
-  const articles = await getCollection('article', ({ data }) => {
+  const articles = await getCollection('articles', ({ data }) => {
     return import.meta.env.PROD ? data.draft !== true : true;
   });
   return rss({
     title: siteTitle,
     description: siteDescription,
     site: context.site,
-    items: articles.map((post) => ({
-      ...post.data,
-      pubDate: post.data.updated || post.data.created,
-      link: `/${post.slug}/`,
+    items: articles.map((article) => ({
+      ...article.data,
+      pubDate: article.data.updated || article.data.created,
+      link: `/${article.id}/`,
     })),
   });
 }
