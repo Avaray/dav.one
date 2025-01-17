3d0ed29e12d26e0e4b0e6040009408dc3c55bf69
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-17 21:05:49 +0100

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSlug from "rehype-slug";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -19,6 +17,11 @@ export default defineConfig({
   vite: {
     // @ts-ignore Shows error because of Beta (?)
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
   },
   prefetch: true,
   image: {
@@ -26,46 +29,5 @@ export default defineConfig({
   },
   markdown: {
     syntaxHighlight: false,
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypeAutolinkHeadings,
-        {
-          behavior: "append",
-          headingProperties: {},
-          properties: {
-            className: "mx-2 my-10",
-          },
-          content: [
-            // {
-            //   type: 'element',
-            //   tagName: 'span',
-            //   properties: {
-            //     id: 'copy-url',
-            //     className: ['i-ph:link-light', 'text-base-content/25', 'hover:text-primary'],
-            //     onClick:
-            //       'copyHeadingUrl(window.location.href, this.parentElement.parentElement.id)',
-            //     title: 'Copy link to this heading',
-            //   },
-            // },
-            {
-              type: "element",
-              tagName: "span",
-              properties: {
-                id: "share-url",
-                className: [
-                  "i-ic:round-share",
-                  "text-base-content/10",
-                  "hover:text-primary",
-                ],
-                onClick:
-                  "shareHeadingUrl(window.location.href, this.parentElement.parentElement.id, this.parentElement.parentElement.textContent)",
-                title: "Share link to this heading",
-              },
-            },
-          ],
-        },
-      ],
-    ],
   },
 });
