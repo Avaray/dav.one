fc02e66a0619c4eb84c40807efa35d22199db270
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-17 21:05:57 +0100

    go

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const articles = defineCollection({
-  loader: glob({ base: './src/content/articles', pattern: '**/*.{md,mdx}' }),
+  loader: glob({ base: '@/content/articles', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
