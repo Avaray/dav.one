399d896f08a626461647f4233f1ee4ee6f5c6dbc
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-19 21:58:58 +0100

    fixed date issues

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,16 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { siteTitle, siteDescription } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { siteDescription, siteTitle } from "../consts";
+
+function rssDateFromTimestamp(timestamp) {
+  // Create a new Date object from the Unix timestamp (converting seconds to milliseconds)
+  const date = new Date(timestamp * 1000);
+  // Return the date in RFC-2822 format, which is required for RSS feeds
+  return date.toUTCString();
+}
 
 export async function GET(context) {
-  const articles = await getCollection('articles', ({ data }) => {
+  const articles = await getCollection("articles", ({ data }) => {
     return import.meta.env.PROD ? data.draft !== true : true;
   });
   return rss({
@@ -12,7 +19,9 @@ export async function GET(context) {
     site: context.site,
     items: articles.map((article) => ({
       ...article.data,
-      pubDate: article.data.updated || article.data.created,
+      pubDate: rssDateFromTimestamp(
+        article.data.updated || article.data.created,
+      ),
       link: `/${article.id}/`,
     })),
   });
