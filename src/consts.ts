048ac38497ada79a2190562f3d81f187d475c6bc
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-19 19:21:44 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const siteTitle = 'Blog about Generative AI, Programming and Web Development';
-export const siteDescription = "Here you will find articles focused on Stable Diffusion, Generative AI and Web Development";
+export const siteTitle = 'Blog about Generative AI, Web Development and Programming';
+export const siteDescription = "Here you will find articles focused on Stable Diffusion, Generative AI, Web Development, Frontend Frameworks and Programming in general";
 export const siteOwner = 'Dawid Wąsowski';
