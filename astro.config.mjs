e28a2df7f5a66c0ab42afda2fa5012d9e5de4c91
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-22 14:44:18 +0100

    fkcu shiki

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,4 +32,7 @@ export default defineConfig({
     // https://docs.astro.build/en/guides/images/#configure-no-op-passthrough-service
     service: passthroughImageService(),
   },
+  markdown: {
+    syntaxHighlight: false,
+  },
 });
