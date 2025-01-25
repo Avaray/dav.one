e5c0c70188dc2b61bdb1effd53c54c1bf3013a7b
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-26 00:04:01 +0100

    Update utils.ts

--- src/scripts/utils.ts
+++ src/scripts/utils.ts
@@ -1,11 +1,7 @@
-// that string will be javascript timestamp
 export const readableDate = (date: string) => {
-  const timestamp = new Date(Number(date));
-  const toReturn = new Date(timestamp).toLocaleDateString('en-us', {
+  return new Date(Number(date)).toLocaleDateString('en-us', {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
   });
-  console.log(toReturn);
-  return toReturn;
 }
