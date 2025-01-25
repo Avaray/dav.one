b9ad8bc729bbab3816a678c8779db914a44b7051
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-25 12:58:02 +0100

    added new fonts

--- uno.config.ts
+++ uno.config.ts
@@ -5,7 +5,12 @@ export const fontList = {
   firamono: 'Fira Mono:400',
   gotu: 'Gotu:400',
   quando: 'Quando:400',
-  // victormono: 'Victor Mono:400,200,100', // Nice font, but not sure if I want to use it
+  victormono: 'Victor Mono:400,200,100', // Nice font, but not sure if I want to use it
+  // New for tests
+  ptserif: 'PT Serif:400',
+  notoserif: 'Noto Serif:400',
+  spectral: 'Spectral:200,300,400',
+  shipporimincho: 'Shippori Mincho:400',
 };
 
 export default defineConfig({
