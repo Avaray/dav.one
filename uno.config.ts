88683b3edbc84225909d4759bc43a6fce6e3affb
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-27 22:56:12 +0100

    removed overkiller shipporimincho font and formateed file with deno
    20Kb font? 😿

--- uno.config.ts
+++ uno.config.ts
@@ -1,26 +1,25 @@
-import { defineConfig } from 'unocss';
-import { presetWebFonts } from 'unocss';
+import { defineConfig } from "unocss";
+import { presetWebFonts } from "unocss";
 
 export const fontList = {
-  firamono: 'Fira Mono:400',
-  gotu: 'Gotu:400',
-  quando: 'Quando:400',
-  victormono: 'Victor Mono:400,200,100', // Nice font, but not sure if I want to use it
+  firamono: "Fira Mono:400",
+  gotu: "Gotu:400",
+  quando: "Quando:400",
+  victormono: "Victor Mono:400,200,100", // Nice font, but not sure if I want to use it
   // New for tests
-  ptserif: 'PT Serif:400',
-  notoserif: 'Noto Serif:400',
-  spectral: 'Spectral:200,300,400',
-  shipporimincho: 'Shippori Mincho:400',
+  ptserif: "PT Serif:400",
+  notoserif: "Noto Serif:400",
+  spectral: "Spectral:200,300,400",
 };
 
 export default defineConfig({
   presets: [
     presetWebFonts({
-      provider: 'google',
-      fonts: fontList
-    })
+      provider: "google",
+      fonts: fontList,
+    }),
   ],
   transformers: [],
   rules: [],
   shortcuts: [],
-})
+});
