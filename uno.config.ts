6aed13b04b0bb5e8612b56b4f3a2fcdab30e24c5
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-28 13:56:11 +0100

    Update uno.config.ts

--- uno.config.ts
+++ uno.config.ts
@@ -7,9 +7,9 @@ export const fontList = {
   quando: "Quando:400",
   victormono: "Victor Mono:400,200,100", // Nice font, but not sure if I want to use it
   // New for tests
-  ptserif: "PT Serif:400",
-  notoserif: "Noto Serif:400",
-  spectral: "Spectral:200,300,400",
+  // ptserif: "PT Serif:400",
+  // notoserif: "Noto Serif:400",
+  // spectral: "Spectral:200,300,400",
 };
 
 export default defineConfig({
